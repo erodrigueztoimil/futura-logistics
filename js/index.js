0dbd31c9dae3cb1ad6c83e55b2a633f917a5c4c3
Ernesto Rodriguez <erodrigueztoimil@gmail.com>
2019-08-24 21:47:32 -0400

    switched from react

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: 25.8628727, lng: -80.3495185};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 16, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
