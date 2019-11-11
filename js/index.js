b87d49f2ab04d37efde691cbe71b8cad17c41669
Ernesto Rodriguez <erodrigueztoimil@gmail.com>
2019-11-11 01:07:57 -0500

    added smooth scrolling

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
 // parallax bg image
 $(window).scroll(function() {
   parallax();
@@ -73,6 +98,21 @@ jQuery(
       $('html').removeClass('overflow');
       menuButton.removeClass('show');
       menuItems.removeClass('show');
+    });
+  }
+)
+
+
+// accross platform smooth scroll handler
+jQuery(
+  function() {
+    var anchor = $('nav a[href^="#"]');
+    var root = $('html, body');
+
+    anchor.click(function() {
+      root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+      }, 10 );
     });
   }
 )
